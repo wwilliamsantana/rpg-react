e677226682358508774920ef535326ba6f054ec3
William Silva <euwilliamsantana@gmail.com>
2022-11-08 17:32:35 -0300

    Movimentação: Ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useEffect } from "react";
 import { Container, Map } from "./App.styles";
 import { Character } from "./components/Character";
 import { useCharacter } from "./hook/useCharacter";
 
 export function App() {
   const char = useCharacter()
+
+  function handleKeyDown(event: KeyboardEvent){
+    switch(event.code){
+      case "ArrowUp":
+      case "KeyW":
+        char.moveUp()
+      break
+      case "ArrowDown":
+      case "KeyS":
+        char.moveDown()
+      break
+      case "ArrowLeft":
+      case "KeyA":
+        char.moveLeft()
+      break
+      case "ArrowRight":
+      case "KeyD":
+        char.moveRight()
+      break
+    }
+  }
+
+  useEffect(()=> {
+    window.addEventListener("keydown", handleKeyDown)
+  }, [])
 
   return (
     <Container>
